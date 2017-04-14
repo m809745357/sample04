fdd37cc3aacf7f1e902fff10041331f0ce0effd7
shen yi fei <m809745357@gmail.com>
2017-04-14 05:16:01 +0000

    Finish user signup

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -2,5 +2,40 @@
 @section('title', 'Login')
 
 @section('content')
-  <h1>Login</h1>
+  <div class="col-md-offset-2 col-md-8">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h5>Signup</h5>
+      </div>
+      <div class="panel-body">
+        @include('shared.errors')
+
+        <form action="{{ route('users.store') }}" method="POST">
+          {{ csrf_field() }}
+
+          <div class="form-group">
+            <label for="name">Name: </label>
+            <input type="text" name="name" class="form-control" value="{{ old('name') }}">
+          </div>
+
+          <div class="form-group">
+            <label for="emial">Email: </label>
+            <input type="text" name="email" class="form-control" value="{{ old('email') }}">
+          </div>
+
+          <div class="form-group">
+            <label for="passowrd">Passowrd: </label>
+            <input type="password" name="password" class="form-control" value="{{ old('email') }}">
+          </div>
+
+          <div class="form-group">
+            <label for="password_confirmation">Confirm Password: </label>
+            <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
+          </div>
+
+          <button type="submit" class="btn btn-primary">Signup</button>
+        </form>
+      </div>
+    </div>
+  </div>
 @stop
